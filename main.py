cde94d0686ddc04fa5692dffa98f16dec17bfb50
Aleks Valentirov <aleks0010v@gmail.com>
2021-05-23 10:30:34 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from smbus2 import SMBus
+from typing import Union
+from os.path import exists
+
+
+class MyI2CBus(SMBus):
+
+    def __init__(self, bus: Union[None, int, str], slave_addr = None) -> None:
+        """
+        :param bus: i2c bus number (e.g. 0 or 1)
+            or an absolute file path (e.g. `/dev/i2c-42`).
+            If not given, a subsequent  call to ``open()`` is required.
+        
+        :param slave_addr: implement for usage as slave device
+        """
+        super().__init__(bus=bus)
+        self.slave_addr = slave_addr
+        self.bus = bus
+        self.devices: dict = {}
+
+    def add_device(self, name: str, addr: int) -> None:
+        if name in self.devices:
+            raise KeyError("Device already exists")
+        else:
+            self.devices[name] = addr
+
+    def update_device(self, name: str, addr: int):
+        if name not in self.devices:
+            raise KeyError("Device does not exist")
+        else:
+            self.devices[name] = addr
+
+    def remove_device(self, name: str):
+        del self.devices[name]
+
+    def load_devices(self, path: str):
+        """
+        uploads devices from config file
+
+        format is:
+        <name> <address with 16 base>
+        ex.:
+        bme280 0x60
+        """
+        if not exists(path):
+            return
+
+        with open(path) as handle:
+            data = handle.readlines()
+        
+        if data:
+            self.devices.clear()
+        for row in data:
+            name, addr = row.split()
+            self.add_device(name, int(addr, base=16))
+
