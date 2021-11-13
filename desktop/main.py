5e7e9434eba0a973a10461da4f3869727d71ef47
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2021-11-13 15:22:13 +0200

    new GUI

--- desktop/main.py
+++ desktop/main.py
@@ -1,10 +1,11 @@
+from sys import argv, exit
+
 from PyQt6.QtWidgets import QApplication
-from GUI.main_app import TestChamberApp
+
+from app import App
 
 
-if __name__ == "__main__":
-    import sys
-    app = QApplication(sys.argv)
-    test_chamber = TestChamberApp()
-    test_chamber.show()
-    sys.exit(app.exec())
+if __name__ == '__main__':
+    qt_app = QApplication(argv)
+    main_app = App()
+    exit(qt_app.exec())
